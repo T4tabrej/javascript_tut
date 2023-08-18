ed34223c6eb64b17ecb2757e7b8957f0238bb3a4
Tabrej <Tabrejhussain7@gmail.com>
2023-08-18 10:36:58 +0530

    addig

---
+++ conditional_statement.js
@@ -0,0 +1,38 @@
+/*
+
+1-if
+2-if-else
+3-else-if
+4-switch
+
+*/
+
+
+// 1.if syntax
+// if(condition){
+
+// }
+
+
+// age=17
+// if (age>18) {
+//     console.log("elligible for vote");
+// }
+
+
+
+// 2-if-else
+// if (condition) {
+    
+// }
+//  else {
+    
+// }
+
+// age=3
+// if (age>18) {
+//    console.log("Elligible for vote"); 
+// } 
+// else {
+//     console.log("Not Elligible for Vote")
+// }
